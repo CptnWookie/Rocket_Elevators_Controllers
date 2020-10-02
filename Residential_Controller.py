3094c1d360b042d91277f24d7d75efa1b37bc0af
Alexandre Leblanc <alexandreleblanc89@gmail.com>
2020-10-01 21:59:25 -0400

    update

--- Residential_Controller.py
+++ Residential_Controller.py
@@ -1,3 +1,4 @@
+
 class Column:
     def __init__(self, floorAmount, elevatorAmount):
         self.floorAmount = floorAmount
@@ -5,51 +6,64 @@ class Column:
         self.elevatorList = []
         self.callButtonList = []
         self.floorRequestButtonList = []
-        self.bestElevator
-
-        i = 1
-        while i <= self.elevatorAmount:
-            print('new Elevator')
-            elevator = Elevator('Elevator' + i, 'idle', 1, 'none', 10, 'closed')
+        
+        print('ELEVATOR LIST :')
+        i = 0
+        while i < self.elevatorAmount:
+            elevator = Elevator('Elevator {}'.format(i + 1))
             self.elevatorList.append(elevator)
+            print(self.elevatorList[i].id)
             i += 1
-
-        print(self.elevatorList)
-
-
-        for floorAmount in range (1,11):
-            print(floorAmount)
-            floorRequestButton = FloorRequestButton(id, floorAmount)
+        
+        print('')
+        print('FLOOR REQUEST BUTTON LIST :')
+        for i in range(10):
+            floorRequestButton = FloorRequestButton(i + 1, self.floorAmount)
             self.floorRequestButtonList.append(floorRequestButton)
+            print('FloorRequestButton', self.floorRequestButtonList[i].id)
 
-        print(self.floorRequestButtonList)
+        print('')
+        print('CALL BUTTON LIST :')
+        for i in range(self.floorAmount):
+            if i == 0:
+                callButton = CallButton (i + 1, 'off', 'up')
+                self.callButtonList.append(callButton)
+                print('CallButton up {}'.format(self.callButtonList[i].id))
+            
+            elif i == 9:
+                callButton = CallButton(i + 1, 'down', 'off')
+                self.callButtonList.append(callButton)
+                print('CallButton Down {}'.format(self.callButtonList[i].id))
 
-        for floorAmount in range(1,11):
+            else:
+                callButton = CallButton(i + 1, 'down', 'up')
+                self.callButtonList.append(callButton)
+                print('CallButton Down|Up {}'.format(self.callButtonList[i].id))
 
+
+            
 
 
 class Elevator:
-    def __init__(self, id, status, position, direction, floor, doors):
-        self.id = id
-        self.status = status
-        self.position = position
-        self.direction = direction
-        self.floor = floor
-        self.doors = doors
+    def __init__(self, _id):
+        self.id = _id
+        self.status = 'idle'
+        self.position = 1
+        self.direction = 'idle'
+        self.floor = None
+        self.doors = 'closed'
         self.requestList = []
 
-
-
 class CallButton:
-    def __init__(self, id, floor, direction):
-        self.id = id
+    def __init__(self, _id, floor, direction):
+        self.id = _id
         self.direction = direction
         self.floor = floor
 
-
-
 class FloorRequestButton:
-    def __init__(self, id, floorAmount):
-        self.id = id
+    def __init__(self, _id, floorAmount):
+        self.id = _id
         self.floorAmount = floorAmount
 
+
+c = Column(10, 2)
