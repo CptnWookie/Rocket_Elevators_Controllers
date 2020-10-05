8fc4553444d926e59cd430d14e821baad07e1ad6
Alexandre Leblanc <alexandreleblanc89@gmail.com>
2020-10-05 15:46:57 -0400

    Commercial Controler C# Created and start coding

---
+++ Commercial_Controller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Rocket_Elevators_Controllers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rocket Elevators Commercial Controller!");
+        }
+
+        class Battery
+        {
+            String columnAmount;
+            String floorAmount;
+            String floorGroup;
+            String elevatorAmountPerColumn;
+            String direction;
+            String floor;
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
