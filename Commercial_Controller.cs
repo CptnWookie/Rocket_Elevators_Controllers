893f82c109159dd5b96a8916afb3ea8d255d4a8e
Alexandre Leblanc <alexandreleblanc89@gmail.com>
2020-10-05 22:58:46 -0400

    Battery Created. Working on Column

--- Commercial_Controller.cs
+++ Commercial_Controller.cs
@@ -1,45 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Rocket_Elevators_Controllers
-{
-    class Program
+    namespace Rocket_Elevators_Controllers
     {
-        static void Main(string[] args)
+        class Commercial_Controller
         {
-            Console.WriteLine("Rocket Elevators Commercial Controller!");
+            static void Main(string[] args)
+            {
+                Battery bat = new Battery(4, 66, 3);
+
+                foreach (Column col in bat.columnList)
+                {
+                    Console.WriteLine("Column" + col.id);
+                }
+
+                /* for (int i = 0; i < bat.columnList.Count; i++)
+                {
+                    Console.WriteLine("iteration {0}, element id {1}", i, bat.columnList[i].id);
+                } */
+
+                Console.WriteLine("Commercial Controller!");
+                Console.ReadKey();
+            }
+
+            class Battery
+            {
+                public int columnAmount;
+                public int floorAmount;
+                public int floorGroup;
+                public int elevatorAmountPerColumn;
+                public string direction;
+                public List<Column> columnList  = new List<Column>();
+                public List<CallButton> callButtonList = new List<CallButton>();
+                public List<FloorGroup> floorGroupList = new List<FloorGroup>();
+
+                
+                public Battery(int _columnAmount, int _floorAmount, int _elevatorAmountPerColumn)
+                {
+                    columnAmount = _columnAmount;
+                    floorAmount = _floorAmount;
+                    elevatorAmountPerColumn = _elevatorAmountPerColumn;
+
+                    for (int i = 1; i <= columnAmount; i++)
+                    {
+                        columnList.Add(new Column(i));
+                        
+                    }
+                }
+            }
+
+            class Column
+            {
+                public int id;
+                
+
+                public Column(int _id)
+                {
+                    id = _id;
+                }
+
+            }
+
+            class Elevator
+            {
+                
+            }
+
+            class CallButton
+            {
+
+            }
+
+            class FloorGroup
+            {
+
+            }
         }
-
-        class Battery
-        {
-            String columnAmount;
-            String floorAmount;
-            String floorGroup;
-            String elevatorAmountPerColumn;
-            String direction;
-            String floor;
-            
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
-}
