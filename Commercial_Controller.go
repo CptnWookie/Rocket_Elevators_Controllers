50e842307cffe13f8b80dc87dcf2f5fe37ad4ced
Alexandre Leblanc <alexandreleblanc89@gmail.com>
2020-10-08 16:10:10 -0400

    Go started

---
+++ Commercial_Controller.go
@@ -0,0 +1,16 @@
+// Go Lang code for Commercial Controller
+package main
+
+import (
+	"fmt"
+)
+
+// Scenario Zone
+func main() {
+	fmt.Println("Hello World")
+}
+
+// Battery ...
+type Battery struct {
+	id int
+}
